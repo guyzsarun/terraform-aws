0f816874a30f5abe6348b8478ce7a647e7dcb931
Sarun Nuntaviriyakul <48861591+guyzsarun@users.noreply.github.com>
2023-12-11 19:42:46 +0100

    Feature: EKS (#2)
    * add eks node
    
    * remove life cycle eks

---
+++ eks.tf
@@ -0,0 +1,79 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.20.0"
+
+
+  cluster_name    = var.eks-config.name
+  cluster_version = var.eks-config.version
+
+  vpc_id     = aws_vpc.main-vpc.id
+  subnet_ids = [ for i in aws_subnet.main-vpc-subnet-private  : i.id]
+
+  cluster_endpoint_public_access = true
+  eks_managed_node_groups = {
+    node-group-1 = {
+      capacity_type  = "SPOT"
+      instance_types = ["t3.small"]
+
+      min_size     = var.eks-config.min_node
+      max_size     = var.eks-config.max_node
+      desired_size = var.eks-config.min_node
+    }
+    node-group-2 = {
+      capacity_type  = "SPOT"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+    }
+  }
+}
+
+locals {
+  kubeconfig = yamlencode({
+    apiVersion      = "v1"
+    kind            = "Config"
+    current-context = "terraform"
+    clusters = [{
+      name = module.eks.cluster_name
+      cluster = {
+        certificate-authority-data = module.eks.cluster_certificate_authority_data
+        server                     = module.eks.cluster_endpoint
+      }
+    }]
+    contexts = [{
+      name = "terraform"
+      context = {
+        cluster = module.eks.cluster_name
+        user    = "terraform"
+      }
+    }]
+    users = [{
+      name = "terraform"
+      user = {
+        exec = {
+          apiVersion = "client.authentication.k8s.io/v1beta1"
+          command= "aws"
+          interactiveMode= "IfAvailable"
+          provideClusterInfo= false
+          args = [
+           "--region",
+           split(":",module.eks.cluster_arn)[3],
+           "eks",
+           "get-token",
+           "--cluster-name",
+           "${module.eks.cluster_name}",
+           "--output",
+           "json"
+          ]
+        }
+      }
+    }]
+  })
+}
+
+resource "local_file" "kubeconfig" {
+  content  = local.kubeconfig
+  filename = "kubeconfig_${module.eks.cluster_name}"
+}

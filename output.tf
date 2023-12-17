bb7657b4d1d5f94c6c8bade0f9a44d63b715fc34
Sarun Nuntaviriyakul <48861591+guyzsarun@users.noreply.github.com>
2023-12-17 16:42:24 +0100

    Feature: Terraform CI (#4)
    * add tf workflow
    
    * update ci
    
    * update exit command
    
    * update return code
    
    * update exit code
    
    * fmt
    
    * update readme

--- output.tf
+++ output.tf
@@ -17,15 +17,15 @@ output "private-vm" {
 
 output "nat-gateway" {
   value = {
-    public_ip = aws_nat_gateway.nat-gw.public_ip
+    public_ip  = aws_nat_gateway.nat-gw.public_ip
     private_ip = aws_nat_gateway.nat-gw.private_ip
   }
 }
 
-output "eks"{
+output "eks" {
   value = {
-    cluster_name = module.eks.cluster_name
-    cluster_endpoint = module.eks.cluster_endpoint
+    cluster_name           = module.eks.cluster_name
+    cluster_endpoint       = module.eks.cluster_endpoint
     get_kubeconfig_command = "aws eks update-kubeconfig --name ${module.eks.cluster_name}"
   }
 }

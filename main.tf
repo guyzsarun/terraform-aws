a780ddcca031ad00169c774f8b4828ff3a83c63b
guyzsarun <sarun.nunt@gmail.com>
2023-12-20 11:26:04 +0100

    security group module

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
+module "vpc" {
+  source   = "./modules/vpc"
+  vpc_name = "module-vpc"
+}
+
 module "bastion" {
   source       = "./modules/vm"
   ssh_key_pair = var.ssh_key_pair
 
   vm_name   = "bastion_vm"
-  subnet_id = aws_subnet.main-vpc-subnet-public[0].id
+  subnet_id = module.vpc.public_subnet_id[0]
 
   init_script = "./helper/init.sh"
 
   security_group_ids = [
-    aws_security_group.allow_ssh.id,
-    aws_security_group.allow_egress.id
+    module.vpc.security_group_allow_ssh,
+    module.vpc.security_group_allow_egress
   ]
 }
 
@@ -18,10 +23,10 @@ module "private_bastion" {
   ssh_key_pair = var.ssh_key_pair
 
   vm_name   = "private_vm"
-  subnet_id = aws_subnet.main-vpc-subnet-private[0].id
+  subnet_id = module.vpc.private_subnet_id[0]
 
   security_group_ids = [
-    aws_security_group.allow_ssh.id,
-    aws_security_group.allow_egress.id
+    module.vpc.security_group_allow_ssh,
+    module.vpc.security_group_allow_egress
   ]
 }

11a452aa44fbdaf8601df4caa3295342fef61dd6
guyzsarun <sarun.nunt@gmail.com>
2023-12-21 16:15:48 +0100

    fmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "k8s" {
   source = "./modules/k8s"
   vpc_id = module.vpc.vpc_id
 
-  subnet_id = module.vpc.private_subnet_id
+  subnet_id  = module.vpc.private_subnet_id
   eks-config = var.eks-config
 }
 

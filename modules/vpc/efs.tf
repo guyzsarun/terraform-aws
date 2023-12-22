c3f8dd7673476062f3650848e1e4231dab01f2d8
guyzsarun <sarun.nunt@gmail.com>
2023-12-22 22:19:10 +0100

    add efs

---
+++ modules/vpc/efs.tf
@@ -0,0 +1,30 @@
+resource "aws_efs_file_system" "private-nfs" {
+  creation_token         = "private-nfs"
+  throughput_mode        = "elastic"
+
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  tags = {
+    Name = "private-nfs-data"
+  }
+}
+
+
+resource "aws_efs_backup_policy" "private-nfs-policy" {
+  file_system_id = aws_efs_file_system.private-nfs.id
+
+  backup_policy {
+    status = "DISABLED"
+  }
+}
+
+resource "aws_efs_mount_target" "private-nfs-mount" {
+  file_system_id  = aws_efs_file_system.private-nfs.id
+  subnet_id       = each.value
+  security_groups = [aws_security_group.allow_vpc_ingress.id]
+
+  for_each = { for index,i in aws_subnet.main-vpc-subnet-private: index => i.id }
+}

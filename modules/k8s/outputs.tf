9f9edcb8c6ccb229fa596c82e277c5b2a3b8e5aa
guyzsarun <sarun.nunt@gmail.com>
2023-12-30 14:26:40 +0100

    add eks output

---
+++ modules/k8s/outputs.tf
@@ -0,0 +1,11 @@
+output "kubeconfig_file" {
+  value = local_file.kubeconfig.filename
+}
+
+output "cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_cert" {
+    value= module.eks.cluster_certificate_authority_data
+}

05a2ef8aa334cc684c1bad637f8d83a7ac210ce0
guyzsarun <sarun.nunt@gmail.com>
2023-12-30 14:22:36 +0100

    add helm provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.24.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.10.1"
+    }
   }
 }
 
@@ -15,6 +19,19 @@ provider "aws" {
   default_tags {
     tags = {
       Managed_by = "terraform"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+
+     host                   = module.k8s.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.k8s.cluster_ca_cert)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.eks-config.name]
+      command     = "aws"
     }
   }
 }

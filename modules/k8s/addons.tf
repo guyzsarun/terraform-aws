afa180a5e169662bdfba2d119ed3600712fb55ce
Sarun Nuntaviriyakul <48861591+guyzsarun@users.noreply.github.com>
2023-12-30 15:29:00 +0100

    Feature: k8s addons (#8)
    * add helm provider
    
    * add eks output
    
    * metrics server
    
    * add depends
    
    * fmt

---
+++ modules/k8s/addons.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "metrics-server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.10.0"
+
+  namespace = "kube-system"
+
+}
